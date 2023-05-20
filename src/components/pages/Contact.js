df950354d0973c325ac64db60371bd38b79c33f0
Brett Hicklin <hicklin.brett@gmail.com>
2023-05-20 15:29:43 -0400

    added validation and error messages

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,25 +3,108 @@ import React, { useState } from 'react';
 
 export default function Contact(){
 
-  const [name, setName]= useState('');
-  const [email, setEmail]= useState('');
-  const [message,setMessage]= useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message,setMessage] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [messageError, setMessageError] = useState('');
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function handleMessageChange(event) {
+    setMessage(event.target.value);
+  }
+
+  function handleNameBlur() {
+    if (name.trim() === '') {
+      setNameError('Name is required');
+    } else {
+      setNameError('');
+    }
+  }
+
+  function handleEmailBlur() {
+    if (email.trim() === '') {
+      setEmailError('Email is required');
+    } else {
+      setEmailError('');
+    }
+  }
+
+  function handleMessageBlur() {
+    if (message.trim() === '') {
+      setMessageError('Message is required');
+    } else {
+      setMessageError('');
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (name.trim() === '') {
+      setNameError('Name is required');
+    }
+
+    if (email.trim() === '') {
+      setEmailError('Email is required');
+    }
+    if (message.trim() === '') {
+      setMessageError('Message is required');
+    }
+
+    if (name.trim() !== '' && email.trim() !== '' && message.trim() !== '') {
+    
+      setName('');
+      setEmail('');
+      setMessage('');
+      console.log('Form submitted!');
+    }
+  }
 
   return(
-    <form>
+    <form onSubmit={handleSubmit}>
       
     <label>
       Name:
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+          onBlur={handleNameBlur}
+          required
+        />
+         {nameError && <div>{nameError}</div>}
     </label>
     <br />
     <label>
       Email:
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          onBlur={handleEmailBlur}
+          required
+        />
+        {emailError && <div>{emailError}</div>}
     </label>
     <label>
       Message:
-      <input type="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+      <input
+          type="message"
+          value={message}
+          onChange={handleMessageChange}
+          onBlur={handleMessageBlur}
+          required
+        />
+        {messageError && <div>{messageError}</div>}
     </label>
     <br />
     <button type="submit">Submit</button>
