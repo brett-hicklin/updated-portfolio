b69ebab889a24f7e094d236d36ad2d429d1bd716
Brett Hicklin <hicklin.brett@gmail.com>
2023-05-20 15:00:11 -0400

    created form for contact section

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,42 @@
 import React, { useState } from 'react';
 
-function Contact(props) {
+
+export default function Contact(){
+
+  const [name, setName]= useState('');
+  const [email, setEmail]= useState('');
+  const [message,setMessage]= useState('');
+
   return(
-    <div>
-        contact
-    </div>
+    <form>
+      
+    <label>
+      Name:
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    </label>
+    <br />
+    <label>
+      Email:
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    </label>
+    <label>
+      Message:
+      <input type="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+    </label>
+    <br />
+    <button type="submit">Submit</button>
+  
+    </form>
   )
 }
 
-export default Contact;
+
+
+
+
+
+
+
+
+
+
