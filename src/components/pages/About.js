d564ee6c3743cbbb81a8d254d6f394293fd1aa5a
Brett Hicklin <hicklin.brett@gmail.com>
2023-05-20 14:14:31 -0400

    added content for about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,38 @@ import React, { useState } from 'react';
 
 export default function About(props) {
   return (
-    <div>
-        about me
-    </div>
+    <section>
+      <h2 id="aboutme" className="border">About Me</h2>
+      <p>I am Brett Hicklin, and I'm currently enrolled in a coding bootcamp where I will learn about
+      <ul class="columnlist">
+        <li>HTML/CSS/Git</li>
+        <li>JavaScript</li>
+        <li>Bootstrap</li>
+        <li>The DOM</li>
+        <li>APIs</li>
+        <li>JQuery</li>
+        <li>AJAX</li>
+        <li>Agile development</li>
+        <li>Node</li>
+        <li>ES6</li>
+        <li>Object-oriented programming</li>
+        <li>Express</li>
+        <li>MySQL</li>
+        <li>MVC paradigm</li>
+        <li>Sequelize</li>
+        <li>Testing</li>
+        <li>Team collaboration</li>
+        <li>Project demonstration and storytelling</li>
+        <li>Progressive Web Apps</li>
+        <li>React</li>
+        <li>NoSQL</li>
+        <li>MERN Stack</li>
+        <li>Computer science fundamentals</li>
+      </ul>
+      </p>
+      <p> My background includes electronics repair and overhaul on nuclear powered submarines
+        as well as calibration of electrical test equipment</p>
+    </section>
   )
 }
 
