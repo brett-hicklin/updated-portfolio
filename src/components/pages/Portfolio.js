0b46daac96c2c3165631b21819ab3c9445258c7c
Brett Hicklin <hicklin.brett@gmail.com>
2023-05-21 16:58:45 -0400

    formatted the portfolio section

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,41 +1,59 @@
-import React, { useState } from 'react';
+import React from 'react';
 import RipeTomatoes from '../../assets/images/RipeTomatoes.png';
 import MMM from '../../assets/images/MMM.png';
 import Jate from '../../assets/images/Jate.png';
 import WeatherDashboard from '../../assets/images/Weather-Dashboard-2.png';
 import Scheduler from '../../assets/images/workday-scheduler.png';
 import PwGenerator from '../../assets/images/password-generator-1.png';
+import '../../Portfolio.css'
 
 function Portfolio() {
-  return(
+  return (
     <div className="flexbox-container">
+      <div className="item-container">
+        <a href="https://jdeshaies.github.io/movie-search-project/index.html" target="_blank">
+          <img src={RipeTomatoes} alt="Ripe Tomates movie search" />
+        </a>
+        <a href="https://github.com/brett-hicklin/movie-search-project" target="_blank" >Ripe Tomatoes Repo</a>
+      </div>
 
-    <a href="https://jdeshaies.github.io/movie-search-project/index.html" target="_blank">
-      <img className="item item1" src={RipeTomatoes} alt="Ripe Tomates movie search" height="250px"
-        width="250px"/>
-    </a>
-    
-    <a href="https://cryptic-brushlands-31143.herokuapp.com/" target="_blank">
-      <img className="item item2" src={MMM} alt="recipe storing website" height="250px" width="200px"/> </a>
-    
-      <a href="https://radiant-mesa-13159.herokuapp.com/" target="_blank">
-        <img className="item item2" src={Jate} alt="text editor" height="150px" width="175px"/> </a>
+      <div className="item-container">
+        <a href="https://cryptic-brushlands-31143.herokuapp.com/" target="_blank">
+          <img src={MMM} alt="recipe storing website" />
+        </a>
+        <a href="https://github.com/brett-hicklin/Mmm-My-Menu-Memory-App" target="_blank">My Menu Memory Repo</a>
+      </div>
 
+      <div className="item-container">
+        <a href="https://radiant-mesa-13159.herokuapp.com/" target="_blank">
+          <img src={Jate} alt="text editor" />
+        </a>
+        <a href="https://github.com/brett-hicklin/text-editor" target="_blank">Just Another Text Editor Repo</a>
+      </div>
 
-    <a href="https://brett-hicklin.github.io/weather-dashboard/" target="_blank">
-      <img className="item item3" src={WeatherDashboard} alt="open book" height="150px" width="175px"/>
-    </a>
+      <div className="item-container">
+        <a href="https://brett-hicklin.github.io/weather-dashboard/" target="_blank">
+          <img src={WeatherDashboard} alt="weather dashboard" />
+        </a>
+        <a href="https://github.com/brett-hicklin/weather-dashboard" target="_blank">Weather Dashboard Repo</a>
+      </div>
 
-    <a href="https://brett-hicklin.github.io/work-day-scheduler/" target="_blank">
-      <img className="item item3" src={Scheduler} alt="open book" height="150px" width="175px"/>
-    </a>
+      <div className="item-container">
+        <a href="https://brett-hicklin.github.io/work-day-scheduler/" target="_blank">
+          <img src={Scheduler} alt="workday scheduler" />
+        </a>
+        <a href="https://github.com/brett-hicklin/work-day-scheduler" target="_blank">Workday Scheduler Repo</a>
+      </div>
 
-    <a href="https://brett-hicklin.github.io/password-generator/" target="_blank">
-      <img className="item item3" src={PwGenerator} alt="open book" height="150px" width="175px"/>
-    </a>
-   
-  </div>
-  )
+      <div className="item-container">
+        <a href="https://brett-hicklin.github.io/password-generator/" target="_blank">
+          <img src={PwGenerator} alt="secure password generator" />
+        </a>
+        <a href="https://github.com/brett-hicklin/password-generator" target="_blank">Password Generator Repo</a>
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
+
