a6cb318594b94b94846316a1724f1cf4bdc75c32
Brett Hicklin <hicklin.brett@gmail.com>
2023-05-21 17:12:29 -0400

    created footer with icons to three profiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { FaGithub, FaLinkedin, FaStackOverflow } from 'react-icons/fa';
+import '../Footer.css';
+
 
 export default function Footer(){
-return(
-    <div>
-        this is a footer
-    </div>
-)
+    return (
+        <footer className="footer">
+          <div className="footer-container">
+            <a href="https://github.com/brett-hicklin" target="_blank" rel="noopener noreferrer">
+              <FaGithub className="icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/brett-hicklin-011042154/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className="icon" />
+            </a>
+            <a href="https://stackoverflow.com/users/21936898/brett18" target="_blank" rel="noopener noreferrer">
+              <FaStackOverflow className="icon" />
+            </a>
+          </div>
+        </footer>
+      );
 }
